82345146f09fe85bfb8c72d5386dceace7849a30
GestaRegum <147824834+GestaRegum@users.noreply.github.com>
2024-04-07 17:49:38 +0300

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -4,11 +4,106 @@ new Swiper('.swiper',{
         el: '.swiper-pagination',
         }
         });
+
+
+let menuOpen = document.getElementById("menu_button");
+
+let menuLinks = document.querySelectorAll('.hover__menu');
+
    
+/*Открываем меню*/
+ let openMenuList = menuOpen.addEventListener("click", function() {
+
+        document.getElementById("menu_button").classList.add("menu_open");
+        document.getElementById("menuList").classList.remove("close");
+        document.getElementById("main").classList.add("blur");
+        document.getElementById("feedback_form").classList.add("close");
+
+    });
+
+/*Закрываем меню нажимая на крестик*/
+document.querySelector('.x').addEventListener("click", function() {
+
+        document.getElementById("menu_button").classList.remove("menu_open");
+        document.getElementById("menuList").classList.add("close");
+        document.getElementById("main").classList.remove("blur");
+       
+});
+
+
+
+/*Закрываем меню нажимая на Esc*/
+window.addEventListener('keydown', (e) => {
+        if (e.key === "Escape") {
+        document.getElementById("menu_button").classList.remove("menu_open");
+        document.getElementById("menuList").classList.add("close");
+        document.getElementById("main").classList.remove("blur");
+}
+});
+
+let removeMenuList = document.addEventListener('click', function(e) {
+
+        let header = document.querySelector('.header');
+
+        if (!header.contains(e.target)) {
+                document.getElementById("menu_button").classList.remove("menu_open");
+                document.getElementById("menuList").classList.add("close");
+                document.getElementById("main").classList.remove("blur");
+              
+        }
+  
+
+});
 
 
 
 
+document.removeEventListener('click', removeMenuList);
+
+
+ /*вызываем окно запроса звонка*/
+ 
+document.querySelector('.phone').addEventListener("click", function () {
+
+        document.getElementById("feedback_form").classList.remove("close");
+        document.getElementById("main").classList.add("blur");
+        document.getElementById("menuList").classList.add("close");
+        document.getElementById("menu_button").classList.remove("menu_open");
+});
+
+document.addEventListener('click', function(e) {
+
+        let callBackForm = document.querySelector('header');
+
+                if (!callBackForm.contains(e.target)) {
+                
+                        document.getElementById("feedback_form").classList.add("close");
+                        document.getElementById("main").classList.remove("blur");
+                }
+
+
+});
+
+
+
+
+
+
+menuLinks.forEach(hover__menu => hover__menu.addEventListener('click', function() {
+        document.getElementById("menu_button").classList.remove("menu_open");
+        document.getElementById("menuList").classList.add("close");
+        document.getElementById("main").classList.remove("blur");
+
+
+}))
+
+
+
+
+
+
+
+/*
 let items = document.querySelectorAll('.item');
 let open = document.querySelector('.open');
 
@@ -21,23 +116,10 @@ open.addEventListener('click', function(evn) {
         items.forEach(item =>    
         item.classList.toggle('hideItemDT'));
 
-        open.classList.toggle('rotate');
+        open.classList.toggle('rotate'); 
 
-});
-
-
-
-    
-  
-
-  
+        
 
 
-
-
-
-
-
-      
-
+});*/
 
